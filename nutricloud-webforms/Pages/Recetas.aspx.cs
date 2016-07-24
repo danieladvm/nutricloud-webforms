839e4451419b2a29befa6ede4b6430ae8f72d2ad
FedeFunes <federicofunes92@gmail.com>
2016-07-24 19:56:52 -0300

    Recetas

---
+++ nutricloud-webforms/Pages/Recetas.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using nutricloud_webforms.Repositories;
+using nutricloud_webforms.DataBase;
+
+namespace nutricloud_webforms.Pages
+{
+    public partial class Recetas : System.Web.UI.Page
+    {
+        private RecetaRepository repository = new RecetaRepository();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<usuario_receta> list = repository.Listar();
+
+            RepeaterRecetas.DataSource = list;
+            RepeaterRecetas.DataBind();
+        }
+
+        public void Ver_Click(object sender, EventArgs e)
+        {
+            string x = "Llego!";
+        }
+    }
+}
---
+++ nutricloud-webforms/Repositories/RecetaRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using nutricloud_webforms.DataBase;
+
+namespace nutricloud_webforms.Repositories
+{
+    public class RecetaRepository
+    {
+        private nutricloudEntities c = new nutricloudEntities();
+
+        public List<usuario_receta> Listar()
+        {
+            var list = (from ur in c.usuario_receta select ur).ToList();
+            return list;
+        }
+    }
+}
