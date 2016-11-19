51581b645ad6527d725df07d87ca40bded013ee8
FedeFunes <federicofunes92@gmail.com>
2016-11-19 15:41:35 -0300

    commit DiarioRepository

--- nutricloud-webforms/Repositories/DiarioRepository.cs
+++ nutricloud-webforms/Repositories/DiarioRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using nutricloud_webforms.Models;
+using nutricloud_webforms.DataBase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -39,6 +41,31 @@ namespace nutricloud_webforms.Repositories
             {
 
                 throw;
+            }
+        }
+
+        public void actualizarDiario(List<Diario> diario)
+        {
+
+            UsuarioCompleto usuario = (UsuarioCompleto)HttpContext.Current.Session["UsuarioCompleto"];
+
+            foreach (Diario d in diario)
+            {
+                if (d.idUsuarioAlimento == "")
+                {
+                    usuario_alimento ua = new usuario_alimento();
+                    DateTime f = DateTime.Parse(d.fecha);
+
+                    ua.id_usuario = usuario.Usuario.id_usuario;
+                    ua.id_alimento = int.Parse(d.idAlimento);
+                    ua.id_comida_tipo = int.Parse(d.tipoDeComida);
+                    ua.cantidad = int.Parse(d.cantidad);
+                    ua.f_ingreso = f;
+
+                    c.usuario_alimento.Add(ua);
+                    c.SaveChanges();
+                }
+
             }
         }
     }
